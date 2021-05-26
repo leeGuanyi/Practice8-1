72559b256916941750fdfe04020ca5f61b519735
LIGUANYI <74758995+leeGuanyi@users.noreply.github.com>
2021-05-26 15:11:54 +0800

    Update Practice8-2.aspx.cs

--- Practice8-1/Practice8-2.aspx.cs
+++ Practice8-1/Practice8-2.aspx.cs
@@ -1,14 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+//------------------------------------------------------------------------------
+// <自動產生的>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生的>
+//------------------------------------------------------------------------------
 
-namespace Practice8_1 {
-    public partial class Practice8_2 : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
-        }
+namespace Practice8_1
+{
+
+
+    public partial class Practice8_2
+    {
+
+        /// <summary>
+        /// form1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// tb_Msg 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_Msg;
+
+        /// <summary>
+        /// RegularExpressionValidator1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
     }
 }
